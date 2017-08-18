b08ae7153e1db80c18007380a9cdc65467a99bb7
fchaxel <fchaxel@8324893d-a7a8-4806-89d4-2f8fea5c7913>
2017-08-18 05:21:19 +0000

    Reads the properties one by one by trying to get PROP_LIST first, by Kevin Smith
    git-svn-id: https://svn.code.sf.net/p/yetanotherbacnetexplorer/code/trunk@244 8324893d-a7a8-4806-89d4-2f8fea5c7913

--- Yabe/MainDialog.cs
+++ Yabe/MainDialog.cs
@@ -1210,48 +1210,66 @@ namespace Yabe
             comm.Retries = 1;       //we don't want to spend too much time on non existing properties
             try
             {
-                // Three mandatory common properties to all objects : PROP_OBJECT_IDENTIFIER,PROP_OBJECT_TYPE, PROP_OBJECT_NAME
+                // PROP_LIST was added as an addendum to 135-2010
+                // Test to see if it is supported, otherwise fall back to the the predefined delault property list.
+                bool objectDidSupplyPropertyList = ReadProperty(comm, adr, object_id, BacnetPropertyIds.PROP_PROPERTY_LIST, ref values);
 
-                // ReadProperty(comm, adr, object_id, BacnetPropertyIds.PROP_OBJECT_IDENTIFIER, ref values)
-                // No need to query it, known value
-                BacnetPropertyValue new_entry = new BacnetPropertyValue();
-                new_entry.property = new BacnetPropertyReference((uint)BacnetPropertyIds.PROP_OBJECT_IDENTIFIER, System.IO.BACnet.Serialize.ASN1.BACNET_ARRAY_ALL);
-                new_entry.value = new BacnetValue[] { new BacnetValue(object_id) };
-                values.Add(new_entry);
-
-                // ReadProperty(comm, adr, object_id, BacnetPropertyIds.PROP_OBJECT_TYPE, ref values);
-                // No need to query it, known value
-                new_entry = new BacnetPropertyValue();
-                new_entry.property = new BacnetPropertyReference((uint)BacnetPropertyIds.PROP_OBJECT_TYPE, System.IO.BACnet.Serialize.ASN1.BACNET_ARRAY_ALL);
-                new_entry.value = new BacnetValue[] { new BacnetValue(BacnetApplicationTags.BACNET_APPLICATION_TAG_ENUMERATED, (uint)object_id.type) };
-                values.Add(new_entry);
-
-                // We do not know the value here
-                ReadProperty(comm, adr, object_id, BacnetPropertyIds.PROP_OBJECT_NAME, ref values);
-
-                // for all other properties, the list is comming from the internal or external XML file
-
-                BacnetObjectDescription objDescr = new BacnetObjectDescription(); ;
-
-                int Idx = -1;
-                // try to find the Object description from the optional external xml file
-                if (objectsDescriptionExternal != null)
-                    Idx = objectsDescriptionExternal.FindIndex(o => o.typeId == object_id.type);
-
-                if (Idx != -1)
-                    objDescr = objectsDescriptionExternal[Idx];
-                else
+                //Used the supplied list of supported Properties, otherwise fall back to using the list of default properties.
+                if (objectDidSupplyPropertyList)
                 {
-                    // try to find from the embedded resoruce
-                    Idx = objectsDescriptionDefault.FindIndex(o => o.typeId == object_id.type);
-                    if (Idx != -1)
-                        objDescr = objectsDescriptionDefault[Idx];
-                }
-
-                if (Idx != -1)
-                    foreach (BacnetPropertyIds bpi in objDescr.propsId)
+                    var proplist = values.Last();
+                    foreach (var enumeratedValue in proplist.value)
+                    {
+                        BacnetPropertyIds bpi = (BacnetPropertyIds)(uint)enumeratedValue.Value;
                         // read all specified properties given by the xml file
                         ReadProperty(comm, adr, object_id, bpi, ref values);
+                    }
+                }
+                else
+                {
+                    // Three mandatory common properties to all objects : PROP_OBJECT_IDENTIFIER,PROP_OBJECT_TYPE, PROP_OBJECT_NAME
+
+                    // ReadProperty(comm, adr, object_id, BacnetPropertyIds.PROP_OBJECT_IDENTIFIER, ref values)
+                    // No need to query it, known value
+                    BacnetPropertyValue new_entry = new BacnetPropertyValue();
+                    new_entry.property = new BacnetPropertyReference((uint)BacnetPropertyIds.PROP_OBJECT_IDENTIFIER, System.IO.BACnet.Serialize.ASN1.BACNET_ARRAY_ALL);
+                    new_entry.value = new BacnetValue[] { new BacnetValue(object_id) };
+                    values.Add(new_entry);
+
+                    // ReadProperty(comm, adr, object_id, BacnetPropertyIds.PROP_OBJECT_TYPE, ref values);
+                    // No need to query it, known value
+                    new_entry = new BacnetPropertyValue();
+                    new_entry.property = new BacnetPropertyReference((uint)BacnetPropertyIds.PROP_OBJECT_TYPE, System.IO.BACnet.Serialize.ASN1.BACNET_ARRAY_ALL);
+                    new_entry.value = new BacnetValue[] { new BacnetValue(BacnetApplicationTags.BACNET_APPLICATION_TAG_ENUMERATED, (uint)object_id.type) };
+                    values.Add(new_entry);
+
+                    // We do not know the value here
+                    ReadProperty(comm, adr, object_id, BacnetPropertyIds.PROP_OBJECT_NAME, ref values);
+
+                    // for all other properties, the list is comming from the internal or external XML file
+
+                    BacnetObjectDescription objDescr = new BacnetObjectDescription(); ;
+
+                    int Idx = -1;
+                    // try to find the Object description from the optional external xml file
+                    if (objectsDescriptionExternal != null)
+                        Idx = objectsDescriptionExternal.FindIndex(o => o.typeId == object_id.type);
+
+                    if (Idx != -1)
+                        objDescr = objectsDescriptionExternal[Idx];
+                    else
+                    {
+                        // try to find from the embedded resoruce
+                        Idx = objectsDescriptionDefault.FindIndex(o => o.typeId == object_id.type);
+                        if (Idx != -1)
+                            objDescr = objectsDescriptionDefault[Idx];
+                    }
+
+                    if (Idx != -1)
+                        foreach (BacnetPropertyIds bpi in objDescr.propsId)
+                            // read all specified properties given by the xml file
+                            ReadProperty(comm, adr, object_id, bpi, ref values);
+                }
             }
             catch { }
 
