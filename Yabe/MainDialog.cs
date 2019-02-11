a35043976bbab2b1f4a4926fec180e726b69cb29
fchaxel <fchaxel@8324893d-a7a8-4806-89d4-2f8fea5c7913>
2019-02-11 12:46:49 +0000

    ReadProperty from Property_List path
    git-svn-id: https://svn.code.sf.net/p/yetanotherbacnetexplorer/code/trunk@318 8324893d-a7a8-4806-89d4-2f8fea5c7913

--- Yabe/MainDialog.cs
+++ Yabe/MainDialog.cs
@@ -1350,13 +1350,20 @@ namespace Yabe
                 //Used the supplied list of supported Properties, otherwise fall back to using the list of default properties.
                 if (objectDidSupplyPropertyList)
                 {
+                    // 3 required properties not in the list
+                    ReadProperty(comm, adr, object_id, BacnetPropertyIds.PROP_OBJECT_NAME, ref values);
+                    ReadProperty(comm, adr, object_id, BacnetPropertyIds.PROP_OBJECT_TYPE, ref values);
+                    ReadProperty(comm, adr, object_id, BacnetPropertyIds.PROP_OBJECT_IDENTIFIER, ref values);
+
                     var proplist = values.Last();
+
                     foreach (var enumeratedValue in proplist.value)
                     {
                         BacnetPropertyIds bpi = (BacnetPropertyIds)(uint)enumeratedValue.Value;
-                        // read all specified properties given by the xml file
+                        // read all specified properties given by the PROP_PROPERTY_LIST, except the 3 previous one
                         ReadProperty(comm, adr, object_id, bpi, ref values);
                     }
+
                 }
                 else
                 {
