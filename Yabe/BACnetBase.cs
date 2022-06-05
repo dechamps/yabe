3c64ed7868728c4fa0671e46a301711067a0d8cc
Etienne Dechamps <etienne@edechamps.fr>
2022-06-05 17:13:38 +0100

    Be more lenient when parsing Time APDUs
    ISO 16484-5-2014 20.2.13 "Encoding of a Time Value" states:
    
    > A value of X'FF' = D'255' in any of the four octets shall indicate
    > that the corresponding value is unspecified and shall be considered a
    > wildcard when matching times. If all four octets = X'FF', the
    > corresponding time may be interpreted as "any" or "don't care."
    
    Yabe doesn't properly handle the case where some of the octets are set
    to 0xFF but not others, which is a valid case according to the above.
    
    This can cause actual problems in practice, for example it makes it
    impossible to list some object properties from Delta Controls DAC-1180E
    controllers which sometimes return Time APDUs with hour = min = sec =
    0xFF but hundreds = 0. This in turn makes it impossible to export the
    Device DB.
    
    This patch fixes the issue by making Yabe more tolerant of unusual Time
    APDUs.

--- Yabe/BACnetBase.cs
+++ Yabe/BACnetBase.cs
@@ -10859,13 +10859,11 @@ namespace System.IO.BACnet.Serialize
             int min = buffer[offset + 1];
             int sec = buffer[offset + 2];
             int hundredths = buffer[offset + 3];
-            if (hour == 0xFF && min == 0xFF && sec == 0xFF && hundredths == 0xFF)
-                btime = new DateTime(1, 1, 1);
-            else
-            {
-                if (hundredths > 100) hundredths = 0;   // sometimes set to 255
-                btime = new DateTime(1, 1, 1, hour, min, sec, hundredths * 10);
-            }
+            if (hour > 24) hour = 0;
+            if (min > 60) min = 0;
+            if (sec > 60) sec = 0;
+            if (hundredths > 99) hundredths = 0;   // sometimes set to 255
+            btime = new DateTime(1, 1, 1, hour, min, sec, hundredths * 10);
             return 4;
         }
 
